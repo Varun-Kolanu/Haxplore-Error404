9b54ebaf08a88b31d28bc1568346115a4da4ae10
Varun-Kolanu <kolanuvarun739@gmail.com>
2024-03-31 18:55:50 +0530

    Removed master backend and frontend

--- master_backend/src/data/database.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = () => {
-    mongoose.connect(process.env.MONGO_URI, {dbName: "haxplore_master"})
-    .then((c) => {
-        console.log(`Database connected with ${c.connection.host}`);
-    })
-    .catch(err => console.log("Error connecting"));
-}
--- master_backend/src/server.js
+++
@@ -1,6 +0,0 @@
-import { app } from "./app.js";
-import { connectDB } from "./data/database.js";
-
-connectDB();
-
-app.listen(process.env.PORT,() => console.log(`listening on http://localhost:${process.env.PORT}`));
